9622bca7e1aa9b7a345a8a6ae5a02793039bbf01
Khodor Mahdi <76620758+khodor0098@users.noreply.github.com>
2022-01-20 16:55:06 +0200

    Delete InvoicesView.java

--- target/classes/invoice/InvoicesView.java
+++
@@ -1,8 +0,0 @@
-package Invoices;
-
-public class InvoicesView {
-	public void printInvoice(String InvoiceNumber,String costumerName,int price,String date,String status) {
-		System.out.println(""+InvoiceNumber+" "+costumerName+" "+price+"$ "+date+" "+status);
-	}
-
-}
