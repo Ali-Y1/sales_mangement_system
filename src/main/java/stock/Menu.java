f54af0e848d8c8370dac35d6c48bdac90326b053
hsnbazi <hassanbazzi19@hotmail.com>
2022-01-26 15:10:52 +0200

    implementation of stock

---
+++ src/main/java/stock/Menu.java
@@ -0,0 +1,18 @@
+package stock;
+
+public class Menu {
+private int addNewProduct=0;
+private String name;
+
+
+public void AddStock(int add,String name ){
+    this.addNewProduct=add;
+    this.name=name;
+}
+
+public void AddProduct(char[] product, int ProductNumber){
+name.copyValueOf(product);
+addNewProduct+=ProductNumber;
+}
+
+}
